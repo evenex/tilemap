aaee70cedd22bc4221b44ef02e86ca4742e8e0b0
evenex <vlevenfeld@gmail.com>
2015-06-13 16:37:40 -0400

    updat

--- source/tilemap.d
+++ source/tilemap.d
@@ -5,31 +5,61 @@ private {
 	import std.math;
 }
 
+version = b;
+
+auto ref each (alias f, R)(auto ref R range)
+{
+	foreach (ref item; range)
+		f (item);
+}
+
 alias fvec = Vector!(2, float);
-alias uvec = Vector!(2, size_t);
+alias ivec = Vector!(2, int);
 
 struct TileMap (Store)
 if (dimensionality!Store == 2)
 {
 	Store data;
 
-	/*
-		limit is an idiom from my interval library, it is like "length" but makes an interval instead
-	*/
 	auto limit (uint i)() const
 	{
-		return data.limit!i;
+		return data.limit!i.fmap!(to!int);
 	}
 
-	ref access (size_t x, size_t y)
+	ref access (int x, int y)
 	{
-		return data[x,y];
+		return data[size_t(x), size_t(y)];
 	}
 
 	auto width () const {return this.limit!0.width;}
 	auto height () const {return this.limit!1.width;}
 
-	mixin SliceOps!(access, width, height, RangeExt);
+	auto pull (S)(S space, Interval!int xs, Interval!int ys)
+	{
+		foreach (i, ref a; data[xs,ys].lexi)
+			a = space.lexi[i];
+	}
+	auto pull (S,T...)(S space, T dims)
+	if (Any!(is_interval, T) && not (All!(is_interval, T)))
+	{
+		import std.typecons : reverse;
+
+		auto dim_range (uint i) {return tuple (dims[i].repeat (dims[(i+1)%2].width), dims[(i+1)%2]);}
+
+		static if (is_interval!(T[0]))
+			auto coords = dim_range!0.expand.zip;
+		else 
+			auto coords = dim_range!1.reverse.expand.zip;
+
+		foreach (i, coord; dim_range)
+			data[coord.expand] = space[i];
+	}
+
+	mixin TransferOps!(pull, SliceOps, access, width, height, RangeExt);
+}
+auto tilemap (S)(S space)
+{
+	return TileMap!S (space);
 }
 
 private auto clamp_to (T)(fvec v, ref TileMap!T tiles)
@@ -37,23 +67,37 @@ private auto clamp_to (T)(fvec v, ref TileMap!T tiles)
 	return vector (
 		v.x.clamp (interval (0, tiles.width)),
 		v.y.clamp (interval (0, tiles.height))
-	).fmap!(compose!(to!size_t, floor));
+	).fmap!(to!int);
 }
 
+struct Neighborhood (S)
+{
+	
+}
+auto neighborhood (T)(ref TileMap!T tiles, fvec pos, float radius)
+{
+	ivec max = (pos + radius + 1).clamp_to (tiles),
+		min = (pos - radius).clamp_to (tiles);
+
+	return map!fvec (orthotope (interval(min.x, max.x), interval(min.y, max.y)))
+		.zip (tiles[min.x..max.x, min.y..max.y]);
+}
+
+auto box_query (alias condition =_=> true, T)(ref TileMap!T tiles, fvec pos, float width)
+{
+	return box_query!condition (tiles, pos, width, width);
+}
 auto box_query (alias condition =_=> true, T)(ref TileMap!T tiles, fvec pos, float width, float height)
 {
 	auto dims = vector (width, height)/2;
 
-	uvec max = (pos + dims + 1).clamp_to (tiles),
+	ivec max = (pos + dims + 1).clamp_to (tiles),
 		min = (pos - dims).clamp_to (tiles);
 
 	return tiles[min.x..max.x, min.y..max.y].lexi.filter!condition;
 }
 auto circle_query (alias condition =_=> true, T)(ref TileMap!T tiles, fvec pos, float radius)
 {
-	uvec max = (pos + radius + 1).clamp_to (tiles),
-		min = (pos - radius).clamp_to (tiles);
-
 	auto intersects (fvec v)
 	{
 		return (pos - fvec (
@@ -63,21 +107,26 @@ auto circle_query (alias condition =_=> true, T)(ref TileMap!T tiles, fvec pos, 
 			.fmap!(a => a^^2)[].sum <= radius^^2;
 	}
 
-	return orthotope (tiles.limit!0, tiles.limit!1)
-		.map!fvec.zip (tiles[])[min.x..max.x, min.y..max.y]
+	return tiles.neighborhood (pos, radius)
 		.lexi.filter!((coord,_) => intersects (coord))
 		.map!((_,tile) => tile)
 		.filter!condition;
 }
+Maybe!(ElementType!T) point_query (T)(ref TileMap!T tiles, Vector2f pos)
+{
+	auto ipos = tuple (pos.x, pos.y).fvec.fmap!(to!int);
 
-/*
-	Randy, regarding the gc:
-	you can't mark main @nogc because of std.conv.to and some nontemplate functions that haven't been marked @nogc
-	but running infognition's gc allocation tracker tool inside of the query functions will verify that no allocations are being triggered
-	too bad @nogc is still such a pain to use
-	if @nogc could be inferred on non-template functions, this would be ideal.
-	unfortunately, i can't template all of the functions i use because i sometimes need to get ParameterType and ReturnType for metaprogramming
-*/
+	if (
+		ipos.x.is_contained_in (tiles[].limit!0) 
+		&& ipos.y.is_contained_in (tiles[].limit!1)
+	)
+
+		return typeof(return)(tiles[ipos.x, ipos.y]);
+	else
+		return typeof(return)(null);
+}
+
+version (a)
 void main ()
 {
 	TileMap!(Array!(int, 2)) testmap;
@@ -103,4 +152,181 @@ void main ()
 	auto c = 2.0;
 	assert (testmap.box_query (0.0.fvec, c, c) == [0,1,4,5]);
 	assert (testmap.circle_query (0.0.fvec, c/2) == [0,1,4]);
+}
+
+import std.stdio;
+import std.array: cache = array;
+
+import Dgame.Window;
+import Dgame.Graphic;
+import Dgame.Math;
+import Dgame.System;
+
+enum ubyte MAP_WIDTH = 12;
+enum ubyte MAP_HEIGHT = 10;
+
+enum ubyte TILE_SIZE = 32;
+enum ubyte MOVE = TILE_SIZE;
+enum ubyte ROTATION = 90;
+enum ubyte GRAVITY = TILE_SIZE / 4;
+
+enum ubyte MAX_FPS = 60;
+enum ubyte TICKS_PER_FRAME = 1000 / MAX_FPS;
+
+struct Tile
+{
+	Sprite sprite;
+	Vector2f pos;
+
+	bool is_start_tile; // REVIEW this sucks
+	bool is_target_tile;
+}
+version (b)
+void main() 
+{
+    Window wnd = Window(MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE, "Dgame Test");
+
+	enum path = `/home/vlad/github/Dgame-Tutorial/`;
+
+    Texture player_tex = Texture(Surface((path)~"Basti-Box.png"));
+    Texture tile_tex = Texture(Surface((path)~"Tile.png"));
+
+    // 0 = empty, a = start, t = (walkable) tile, b = brittle tile, z = target
+	Tile assign_tile (char c, Vector2f pos)
+    {
+		auto dims () {return pos * TILE_SIZE;}
+
+        if (c == 't')
+            return Tile (new Sprite(tile_tex, dims), pos);
+        else if (c == 'a')
+            return Tile (null, pos, true, false);
+        else if (c == 'z')
+			return Tile (null, pos, false, true);
+
+		return Tile.init;
+    }
+
+	auto tmap = tilemap (
+		[
+			'0', 'a', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+			'0', 't', 't', 't', 't', 't', '0', '0', 't', 't', 't', '0',
+			'0', '0', '0', 't', 't', 't', 't', '0', '0', '0', '0', '0',
+			'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+			't', 't', '0', '0', '0', '0', 't', 't', 't', 't', '0', 't',
+			'0', '0', '0', '0', '0', 't', 't', 't', '0', '0', 't', 't',
+			'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+			'0', 't', 't', 't', 't', '0', '0', '0', '0', '0', '0', '0',
+			'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'z',
+			'0', '0', '0', 't', 't', 't', 't', 't', 't', 't', 't', 't',
+		]
+		.laminate (MAP_WIDTH, MAP_HEIGHT)
+		.zip (map!Vector2f (Nat[0..MAP_WIDTH].by (Nat[0..MAP_HEIGHT])))
+		.map!assign_tile
+		.array
+	);
+
+    Tile[] tiles = tmap[].lexi.cache;
+
+    Spritesheet player = new Spritesheet(player_tex, Rect(0, 0, 32, 32));
+    player.setPosition(tmap[].lexi.filter!(t => t.is_start_tile).front.pos);
+    player.setRotationCenter(16, 16);
+
+    Font fnt = Font((path)~"samples/font/arial.ttf", 12);
+    Text fps = new Text(fnt);
+    Text pos_txt = new Text(fnt);
+	fps.setPosition(MAP_WIDTH * TILE_SIZE - 96, 4);
+
+    StopWatch sw;
+    StopWatch sw_fps;
+
+    bool running = true;
+
+    Event event;
+    while (running) {
+        wnd.clear();
+
+        fps.format("FPS: %d", sw_fps.getCurrentFPS());
+
+        if (sw.getElapsedTicks() > TICKS_PER_FRAME) {
+            sw.reset();
+
+			auto ground_contact = tmap.neighborhood (
+				player.getPosition().tupleof.fvec / MOVE, 2
+			).unzip[1]
+				[$/2, 2].sprite is null;
+
+			auto center (T)(Interval!(T,T) ival)
+			{
+				return ival.left + ival.width/2f;
+			}
+
+			auto xxx = tmap.neighborhood (
+				player.getPosition().tupleof.fvec / MOVE, 2
+			).unzip[1];
+			writeln (player.getPosition().tupleof.fvec / MOVE, ` `, [center(xxx.domain[].bounds[0]), center(xxx.domain[].bounds[1])]);
+			writeln ("\t", xxx.domain[].bounds[0], ` `, xxx.domain[].bounds[1]);
+
+			if (!ground_contact)
+				player.move(0, GRAVITY);
+            
+            while (wnd.poll(&event)) {
+                switch (event.type) {
+                    case Event.Type.Quit:
+                        writeln("Quit Event");
+                        running = false;
+                    break;
+                        
+                    case Event.Type.KeyDown:
+                        writeln("Pressed key ", event.keyboard.key);
+                        
+                        if (event.keyboard.key == Keyboard.Key.Esc)
+                            running = false;
+                        else if (ground_contact) {
+                            switch (event.keyboard.key) {
+                                case Keyboard.Key.Left:
+                                    player.move(MOVE * -1, 0);
+                                    player.rotate(ROTATION * -1);
+                                    writeln(player.getRotation());
+                                    player.selectFrame(0);
+                                break;
+                                case Keyboard.Key.Right:
+                                    player.move(MOVE, 0);
+                                    player.rotate(ROTATION);
+                                    writeln(player.getRotation());
+                                    player.selectFrame(1);
+                                break;
+                                default: break;
+                            }
+                        }
+                    break;
+                        
+                    default: break;
+                }
+            }
+
+            if (tmap.point_query (player.getPosition()).fmap!(tile => tile.is_target_tile).to_list[0]) {
+                wnd.push(Event.Type.Quit);
+                writeln("You've won!");
+            } else {
+                const Vector2f pos = player.getPosition();
+                if (pos.x > (MAP_WIDTH * TILE_SIZE) ||
+                    pos.x < 0 ||
+                    pos.y > (MAP_HEIGHT * TILE_SIZE) ||
+                    pos.y < 0)
+                {
+                    writeln("You've lost!");
+                    wnd.push(Event.Type.Quit);
+                }
+            }
+        }
+
+        wnd.draw(fps);
+        wnd.draw(pos_txt);
+
+        tmap[].lexi.filter!(tile => tile.sprite !is null).each!(tile => wnd.draw (tile.sprite));
+
+        wnd.draw(player);
+
+        wnd.display();
+    }
 }
